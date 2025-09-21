809ca58c5b6105068b3df699abc0c48d0a40e7aa
ronganam-sf <ron.ganam@silverfort.com>
2025-09-22 00:48:28 +0300

    fix deprecated homeassistant methods

--- __init__.py
+++ __init__.py
@@ -15,9 +15,9 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN] = entry.data
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
